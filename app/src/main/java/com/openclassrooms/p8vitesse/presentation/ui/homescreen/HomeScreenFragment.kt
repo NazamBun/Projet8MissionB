a5e804160b1c49c5d7e6bbe07982b0a72c8cb46e
Bun Nazam <bun.nazam@gmail.com>
2024-11-24 19:29:38 +0100

    fix some bugs

--- app/src/main/java/com/openclassrooms/p8vitesse/presentation/ui/homescreen/HomeScreenFragment.kt
+++ app/src/main/java/com/openclassrooms/p8vitesse/presentation/ui/homescreen/HomeScreenFragment.kt
@@ -5,7 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.openclassrooms.p8vitesse.R
+import com.openclassrooms.arista.R
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
